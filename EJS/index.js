8e261efb8a0132b2e70f22b8729a4d84e8b855a3
AmriteshRaj123 <pathakamritesh6@gmail.com>
2026-06-17 17:10:22 +0530

    update of EJS file

--- EJS/index.js
+++ EJS/index.js
@@ -4,11 +4,15 @@ const path = require("path");
 const app = express();
 const port = 8080;
 
-// EJS setup
+// Import JSON data
+const data = require("./data.json");
+
+// EJS Setup
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-// Route
+
+// Home Route
 app.get("/", (req, res) => {
     res.render("home", {
         name: "Amritesh",
@@ -16,6 +20,45 @@ app.get("/", (req, res) => {
     });
 });
 
+
+// User Profile Route
+app.get("/user", (req, res) => {
+
+    const user = {
+        username: "amritesh",
+        followers: 500,
+        following: 200
+    };
+
+    res.render("profile", user);
+});
+
+
+// Roll Dice Route
+app.get("/rolldice", (req, res) => {
+
+    let diceVal = Math.floor(Math.random() * 6) + 1;
+
+    res.render("rolldice", { diceVal });
+});
+
+
+// Instagram Dynamic Route
+app.get("/:animal", (req, res) => {
+
+    let { animal } = req.params;
+
+    let instaData = data[animal];
+
+    if (!instaData) {
+        return res.send("Profile not found");
+    }
+
+    res.render("instagram", { instaData });
+});
+
+
+// Start Server
 app.listen(port, () => {
     console.log(`Server started at port ${port}`);
 });
