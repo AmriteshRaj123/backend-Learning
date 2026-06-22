addf44ea613223d545bf1d0e658cf9ac6736181f
AmriteshRaj123 <pathakamritesh6@gmail.com>
2026-06-22 23:57:48 +0530

    update code

--- RestApi-class/index.js
+++ RestApi-class/index.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const path = require('path');
 
+const { v4: uuidv4} = require('uuid');
+
+ //uuidv4(); // ⇨ 'b18794e8-5d0d-417c-b361-ba38e78411b4'
+
+
+
+
 const app = express();
 const port = 3000;
 
@@ -19,21 +26,25 @@ app.set('views', path.join(__dirname, 'views'));
 
 let posts = [
     {
+        id : uuidv4(),
         username: "Amritesh Raj",
         content: "I love Coding"
     },
 
     {
+        id : uuidv4(),
         username: "Shyam",
         content: "I love Coding too"
     },
 
     {
+        id : uuidv4(),
         username: "Ayushi",
         content: "I love watching movies"
     },
 
     {
+        id : uuidv4(),
         username: "Aman Raj",
         content: "I love Coding and watch cricket matches"
     },
@@ -45,29 +56,65 @@ app.get('/posts', (req, res) => {
    res.render("index.ejs" , {posts});
 });
 
-// app.post('/posts', (req, res) =>{
-//     let { username, content} = req.body;
 
-//     posts.push({ username, content});
-//     res.send("post request received");
-//     res.redirect("/posts");
-//      res.redirect("/posts");
-// })
 
 app.post("/posts", (req, res) => {
     let { username, content } = req.body;
+    let id = uuidv4();
 
     posts.push({
+        id,
         username,
         content
     });
-
     res.redirect("/posts");
 });
+
+
 
 app.get("/posts/news", (req, res) => {
     res.render("news.ejs");
 });
+
+app.get("/posts/:id", (req, res) => {
+    let { id } = req.params;
+    let post = posts.find((post) => post.id === id);
+
+    if (!post) {
+        return res.send("Post not found");
+    }
+
+    res.render("show.ejs", { post });
+});
+
+
+app.get("/posts/news", (req, res) => {
+    res.render("news.ejs");
+});
+
+
+app.patch("/posts/:id", (req, res)=> {
+    let { id } = req.params;
+    let newContent = req.body.content;
+    let post = posts.find((post) => post.id === id);
+    post.content = newContent;
+    console.log(post);
+    res.send("patch request working");
+});
+
+
+app.get("/posts/:id/edit", (req, res) => {
+    let { id } = req.params;
+
+    let post = posts.find((post) => post.id === id);
+
+    if (!post) {
+        return res.send("Post not found");
+    }
+
+    res.render("edit.ejs", { post });
+});
+
 
 app.listen(port, () => {
     console.log(`Server started at port ${port}`);
